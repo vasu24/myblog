95f42fd475a4d32dba21071fac67f3b3cc25648b
vasu24 <vasulak@gmail.com>
2024-02-13 15:13:32 +0530

    hiding main menu in profile

--- pages/3_My Profile.py
+++ pages/3_My Profile.py
@@ -8,6 +8,14 @@ image_aicert_form = Image.open("images/aicert.png")
 image_redhat_form = Image.open("images/redhat.png")
 
 
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
+
+
 url="https://www.credly.com/organizations/brightest/"
 redhaturl="https://www.redhat.com/en"
 
